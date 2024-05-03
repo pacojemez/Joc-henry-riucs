b434563bcf2f36f4c12b93b7f55cf529d4b409f9
pacojemez <165767842+pacojemez@users.noreply.github.com>
2024-05-03 13:46:36 +0200

    animacio slime

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,9 +4,9 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	move_and_slide()
-
+	$"Animació slime".play("idleandjump")
 
 func _on_area_2d_body_entered(body):
 	if body == $".."/"Jugador principal":
 		$".."/"Jugador principal".vida += -1
-	
+
