def1654117e930c0e8f20361627865e4b56df7c3
SaliouCiss300 <165768150+SaliouCiss300@users.noreply.github.com>
2024-05-10 14:55:43 +0200

    porta
    clau + e = animacio porta obrint

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -1,6 +1,6 @@
 extends Area2D
-
-
+var has_arrived = 0
+var j = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,7 +14,20 @@ func _process(delta):
 func _on_body_entered(body):
 	if body == $"../../Jugador principal":
 		if $"../../Jugador principal".has_key == true:
-			$"../AnimatedSprite2D".play("porta obrint")
-			$"../CollisionShape2D".queue_free()
-			$".".queue_free()
-			$Label.text = "h"
+			
+			has_arrived = 1
+
+
+func _on_body_exited(body):
+	if body == $"../../Jugador principal":
+		has_arrived = 0
+		$Label.text = ""
+func _physics_process(delta):
+	if has_arrived == 1:
+		$Label.text = "press E"
+		if Input.is_action_just_pressed("e"):
+			j = 1
+	if j == 1:
+		$"../AnimatedSprite2D".play("porta obrint")
+		$"../CollisionShape2D".queue_free()
+		$".".queue_free()
