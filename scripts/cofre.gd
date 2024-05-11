c96e5b86ee7c02fba46cb800178e56fd62167e72
MALACRIAY <mail.malac14@gmail.com>
2024-05-11 21:31:41 +0200

    Cofres
    Els cofres ya funcionen
    P.D.
    va mes petada aquesta merda joer

--- scripts/cofre.gd
+++ scripts/cofre.gd
@@ -1,29 +1,34 @@
 extends CharacterBody2D
-var i = 0
-var j = 0
+var ch_in = false
+var opened = 0
+var collectable = false
+@onready var texto = $TextEdit
 @onready var cofre_a := $"Animació cofre"
-
+@onready var object = $"../AnimatedSprite2D"
 func _physics_process(delta):
-	# Add the gravity.
-	if i == 1:
-		if Input.is_action_just_pressed("e"):
-			$TextEdit.visible = false
-			j = 1
-	if j == 1:
-		cofre_a.play("default")
-		if cofre_a.frame ==2:
-			$Sprite2D.visible = true
-			$TextEdit.visible = true
-			$"./AnimationPlayer".play("big")
-			if Input.is_action_just_pressed("e"):
-				$TextEdit.visible = false
-				$"../Jugador principal".has_torch = true
-				$AnimationPlayer.play_backwards("torch")
-	if $Sprite2D.visible == true:
-		cofre_a.play("stop")
+	abrir()
+	if opened == 2 and Input.is_action_just_pressed("e"):
+			$"../AnimatedSprite2D".visible = false
+			texto.visible = false
 func _on_area_2d_body_entered(body):
-	i= 1 # Replace with function body.
-	$TextEdit.visible = true
+	ch_in = true 
+	texto.visible = true
 func _on_area_2d_body_exited(body):
-	i= 0 # Replace with function body.
-	$TextEdit.visible = false # Replace with function body.
+	ch_in = false 
+	texto.visible = false
+	
+func abrir():
+	if ch_in:
+		if Input.is_action_just_pressed("e") and opened == 0:
+			texto.visible = false
+			opened = 1
+		if opened == 1:
+			cofre_a.play("default")
+			if cofre_a.frame ==2:
+				cofre_a.play("stop")
+				$Sprite2D.visible = true
+				texto.visible = true
+				texto.text = "press E to collect"
+				$"../AnimationPlayer".play("grow")
+				opened = 2
+
