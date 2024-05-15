4e4c224cbf8de677fe01b4b9efc0c126f6ee1d6e
pacojemez <165767842+pacojemez@users.noreply.github.com>
2024-05-15 18:12:48 +0200

    sonido tirachinas

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -1,6 +1,7 @@
 extends Area2D
 var has_arrived = 0
 var j = 0
+var timer = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -25,8 +26,15 @@ func _physics_process(delta):
 	if has_arrived == 1:
 		$Label.text = "press E"
 		if Input.is_action_just_pressed("e"):
-			j = 1
+			if not $ficarclau.playing:
+				$ficarclau.play()
+				j = 1
 	if j == 1:
-		if $"../AnimatedSprite2D".frame == 5:
-			$"../CollisionShape2D".queue_free()
-			$".".queue_free()
+		timer -= delta
+		if timer <= 0:
+			if not $sonidoporta.playing:
+				$sonidoporta.play()
+			$"../AnimatedSprite2D".play("porta obrint")
+			if $"../AnimatedSprite2D".frame == 5:
+				$"../CollisionShape2D".queue_free()
+				$".".queue_free()
