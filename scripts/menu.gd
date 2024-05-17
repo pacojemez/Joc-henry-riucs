54fdf0008da41ee6b4a822b3d4dc3b34e5d55e09
MALACRIAY <165767718+MALACRIAY@users.noreply.github.com>
2024-05-17 14:24:15 +0200

    buttons
    button objectius + button de sortir en el menu inicial + button tornar enrrere en objectius

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,3 +1,11 @@
 extends Node
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/introducció.tscn")
+
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/objectius.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
